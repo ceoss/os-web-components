2b6772979040c0e400010420d6fe842fbc748e5e
Evelyn Hathaway <git@evelynhathaway.com>
2015-08-23 22:11:13 -0700

    KDE-Breeze JavaScript Function, onload, Can Tab Past Disabled

--- KDE/Breeze/5.0/kde-breeze.js
+++ KDE/Breeze/5.0/kde-breeze.js
@@ -1,4 +1,16 @@
-buttons = document.getElementsByClassName('kde-breeze-button');
-for (var i = buttons.length - 1; i >= 0; i--) {
-	buttons[i].tabIndex = "0";
-};
+function kdeBreezeIni() {
+	var buttons = document.getElementsByClassName('kde-breeze-button');
+	for (var i = buttons.length - 1; i >= 0; i--) {
+		if (buttons[i].classList.contains("disabled")) {
+			if (buttons[i].tagName = "button") {
+				buttons[i].tabIndex = "-1";
+			}
+		} else {
+			buttons[i].tabIndex = "0";
+		};
+	};
+}
+window.addEventListener("load", function load(event){
+	window.removeEventListener("load", load, false);
+	kdeBreezeIni();  
+},false);
