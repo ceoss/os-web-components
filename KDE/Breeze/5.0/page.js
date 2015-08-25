83030aa7491bc4189d5ef79c23f567a8be00dd7f
Evelyn Hathaway <git@evelynhathaway.com>
2015-08-24 18:35:58 -0700

    All Code Modal
    Issue with header size.

--- KDE/Breeze/5.0/page.js
+++ KDE/Breeze/5.0/page.js
@@ -1,40 +1,76 @@
 function getCodeForButton(event) {
 	alert(event.currentTarget.innerHTML);
 }
-function startCode() {
-	selectCodeTrigger.removeEventListener("click", startCode, false);
+
+// Select Code
+function startSelectCode() {
+	endAllCode();
+	selectCodeTrigger.removeEventListener("click", startSelectCode, false);
 	selectCodeTrigger.childNodes[0].style.color = "#2196f3";
 	// Code Screen
-	var codeScreen = document.createElement("div");
-	codeScreen.className = "code-screen";
-	document.body.appendChild(codeScreen);
-	codeScreen.addEventListener('click', function() {endCode()}, false);
+	codeScreen.style.backgroundColor = "rgba(0,0,0,0.5)";
+	codeScreen.style.pointerEvents = "all";
+	codeScreen.addEventListener('click', endSelectCode, false);
 	// Buttons
 	var buttons = document.getElementsByClassName('kde-breeze-button-code');
 	for (var i = buttons.length - 1; i >= 0; i--) {
 		buttons[i].classList.add("code");
 		buttons[i].addEventListener('click', getCodeForButton, false);
 	};
-	selectCodeTrigger.addEventListener("click", endCode, false);
+	selectCodeTrigger.addEventListener("click", endSelectCode, false);
 }
-function endCode() {
-	selectCodeTrigger.removeEventListener("click", endCode, false);
+function endSelectCode() {
+	selectCodeTrigger.removeEventListener("click", endSelectCode, false);
 	selectCodeTrigger.childNodes[0].style.color = "#fff";
 	// Code Screen
-	var codeScreen = document.getElementsByClassName('code-screen')[0];
-	codeScreen.remove();
+	codeScreen.style.backgroundColor = "rgba(0,0,0,0)";
+	codeScreen.style.pointerEvents = "none";
+	codeScreen.removeEventListener('click', endSelectCode, false);
 	// Buttons
 	var buttons = document.getElementsByClassName('kde-breeze-button-code');
 	for (var i = buttons.length - 1; i >= 0; i--) {
 		buttons[i].classList.remove("code");
 		buttons[i].removeEventListener('click', getCodeForButton, false);
 	};
-	selectCodeTrigger.addEventListener("click", startCode, false);
+	selectCodeTrigger.addEventListener("click", startSelectCode, false);
 }
+
+// All Code
+function startAllCode() {
+	endSelectCode();
+	// Listeners
+	codeScreen.addEventListener('click', endAllCode, false);
+	allCodeTrigger.addEventListener("click", endAllCode, false);
+	allCodeTrigger.removeEventListener("click", startAllCode, false);
+	// Tigger
+	allCodeTrigger.childNodes[0].style.color = "#2196f3";
+	// Code Screen
+	codeScreen.style.backgroundColor = "rgba(0,0,0,0.5)";
+	codeScreen.style.pointerEvents = "all";
+	allCode.style.top = "12.5%";
+	allCode.style.position = "relative";
+}
+function endAllCode() {
+	// Listeners
+	codeScreen.removeEventListener('click', endAllCode, false);
+	allCodeTrigger.addEventListener("click", startAllCode, false);
+	allCodeTrigger.removeEventListener("click", endAllCode, false);
+	// Tigger
+	allCodeTrigger.childNodes[0].style.color = "#fff";
+	// Code Screen
+	codeScreen.style.backgroundColor = "rgba(0,0,0,0)";
+	codeScreen.style.pointerEvents = "none";
+	allCode.style.top = "100%";
+	allCode.style.position = "fixed";
+}
+
 window.addEventListener("load", function load(event){
-	selectCodeTrigger = document.getElementById("selectCodeTrigger");
-	allCodeTrigger = document.getElementById("allCodeTrigger");
 	window.removeEventListener("load", load, false);
-	selectCodeTrigger.addEventListener("click", startCode, false);
+	selectCodeTrigger = document.getElementById("selectCodeTrigger");
+	selectCodeTrigger.addEventListener("click", startSelectCode, false);
+	allCodeTrigger = document.getElementById("allCodeTrigger");
+	allCodeTrigger.addEventListener("click", startAllCode, false);
+	allCode = document.getElementById("allCode");
+	codeScreen = document.getElementsByClassName("code-screen")[0];
 	document.getElementsByClassName("code-sidebar")[0].style.left = "0"
 },false);
